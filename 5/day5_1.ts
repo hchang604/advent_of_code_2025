02b5035efd00b129b1267349e230a8414bf8e343
hchang604 <hchang19948@gmail.com>
2025-12-15 20:00:01 -0800

    update day 5

--- 5/day5_1.ts
+++ 5/day5_1.ts
@@ -1,37 +1,36 @@
 import { readFile } from 'fs/promises';
 
-async function readTextFile(): Promise<{ idRanges: string[], ids: string[] }> {
+async function readTextFile(): Promise<{ idRanges: string[]; ids: string[] }> {
   const content = await readFile('data.txt', 'utf8');
 
   const splitContent = content.split(/\r?\n/);
 
   const blankLineIndex = splitContent.indexOf('');
-  
+
   const idRanges = splitContent.slice(0, blankLineIndex);
   const ids = splitContent.slice(blankLineIndex + 1);
 
-  return { idRanges, ids};
+  return { idRanges, ids };
 }
 
-
 async function solution() {
-    const { idRanges, ids } = await readTextFile();
+  const { idRanges, ids } = await readTextFile();
 
-    let freshIDsCount = 0;
+  let freshIDsCount = 0;
 
-    for(const id of ids) {
-        for(const range of idRanges) {
-            const min = Number(range.split('-')[0]);
-            const max = Number(range.split('-')[1]);
+  for (const id of ids) {
+    for (const range of idRanges) {
+      const min = Number(range.split('-')[0]);
+      const max = Number(range.split('-')[1]);
 
-            if(Number(id) >= min && Number(id) <= max) {
-                freshIDsCount++;
-                break;
-            }
-        }
+      if (Number(id) >= min && Number(id) <= max) {
+        freshIDsCount++;
+        break;
+      }
     }
+  }
 
-    console.log(freshIDsCount);
+  console.log(freshIDsCount);
 }
 
 solution();
--- 5/day5_2.ts
+++ 5/day5_2.ts
@@ -1,130 +1,94 @@
 import { readFile } from 'fs/promises';
 
-async function readTextFile(): Promise<{ idRanges: string[], ids: string[] }> {
+async function readTextFile(): Promise<{ idRanges: string[]; ids: string[] }> {
   const content = await readFile('data.txt', 'utf8');
 
   const splitContent = content.split(/\r?\n/);
 
   const blankLineIndex = splitContent.indexOf('');
-  
+
   const idRanges = splitContent.slice(0, blankLineIndex);
   const ids = splitContent.slice(blankLineIndex + 1);
 
-  return { idRanges, ids};
+  return { idRanges, ids };
 }
 
-function updateRanges(ranges: string[], range: string): string[] {
-    const min = Number(range.split('-')[0]);
-    const max = Number(range.split('-')[1]); 
+function getIds(
+  ranges: Array<string | undefined>,
+  currentRange: string,
+  count: number
+) {
+  let newCount = count;
+  let largestRange = currentRange;
+  let largestRangeValue = 0;
 
-    const isRangeOutsideAll = ranges.every(r => {
-        const rMin = Number(r.split('-')[0]);
-        const rMax = Number(r.split('-')[1]);
-        return max < rMin && min < rMin || min > rMax && max > rMax;
-    });
+  for (let i = 0; i < ranges.length; i++) {
+    const range = ranges[i];
 
-    if(isRangeOutsideAll) {
-        ranges.push(range);
-        return ranges;
+    if (range) {
+      const min = range.split('-')[0];
+      const max = range.split('-')[1];
+
+      const value = Number(max) - Number(min) + 1;
+
+      if (largestRangeValue < value) {
+        largestRangeValue = value;
+        largestRange = range;
+      }
     }
+  }
 
-    for(let i = 0; i < ranges.length; i++) {
-        const currentMin = Number(ranges[i].split('-')[0]);
-        const currentMax = Number(ranges[i].split('-')[1]);
+  newCount = newCount + largestRangeValue;
 
-        if(min < currentMin && max > currentMax) {
-            // New range engulfs current range
-            ranges[i] = range;
-        } else if(min < currentMin && max <= currentMax && max >= currentMin) {
-            // New range overlaps the start of current range
-            ranges[i] = `${min}-${currentMax}`;
-        } else if(min >= currentMin && max > currentMax && min <= currentMax) {
-            // New range overlaps the end of current range
-            ranges[i] = `${currentMin}-${max}`;
-        }
+  for (let i = 0; i < ranges.length; i++) {
+    const range = ranges[i];
+
+    if (range !== undefined && range !== largestRange) {
+      const largestRangeMin = Number(largestRange.split('-')[0]);
+      const largestRangeMax = Number(largestRange.split('-')[1]);
+
+      const min = Number(range.split('-')[0]);
+      const max = Number(range.split('-')[1]);
+
+      if (
+        min < largestRangeMin &&
+        max >= largestRangeMin &&
+        max <= largestRangeMax
+      ) {
+        ranges[i] = `${min}-${largestRangeMin - 1}`;
+      }
+
+      if (
+        min >= largestRangeMin &&
+        min <= largestRangeMax &&
+        max > largestRangeMax
+      ) {
+        ranges[i] = `${largestRangeMax + 1}-${max}`;
+      }
+
+      if (min >= largestRangeMin && max <= largestRangeMax) {
+        ranges[i] = undefined;
+      }
     }
+  }
 
-    return ranges;
-}
+  ranges = ranges.filter(
+    (range) => range !== undefined && range !== largestRange
+  );
 
-function findDuplicateMin(ranges: string[]): string {
-    const seen = new Set<string>();
-    
-    for(const range of ranges) {
-        const min = range.split('-')[0];
-        if(seen.has(min)) {
-            return min;
-        }
-        seen.add(min);
-    }
-
-    return '';
-}
-
-function findDuplicateMax(ranges: string[]): string {
-    const seen = new Set<string>();
-    
-    for(const range of ranges) {
-        const max = range.split('-')[1];
-        if(seen.has(max)) {
-            return max;
-        }
-        seen.add(max);
-    }
-
-    return '';
-}
-
-function mergeRanges(ranges: string[]): string[] {
-    const duplicateMin = findDuplicateMin(ranges);
-
-    let biggestMax = '-1';
-
-    if(duplicateMin) {
-        const newRange = ranges.reduce((acc: string[], cum: string) => {
-            const min = cum.split('-')[0];
-            
-            if(min !== duplicateMin) {
-                return [...acc, cum]
-            }
-
-            const max = cum.split('-')[1];
-
-            if(Number(max) > Number(biggestMax)) {
-                biggestMax = max;
-            }
-
-            return [...acc]
-        }, [])
-
-        newRange.push(`${duplicateMin}-${biggestMax}`);
-
-        return mergeRanges(newRange);
-    }
+  if (ranges.length === 0) {
+    return newCount;
+  } else {
+    return getIds(ranges, largestRange, newCount);
+  }
 }
 
 async function solution() {
-    const { idRanges } = await readTextFile();
+  const { idRanges } = await readTextFile();
 
-    let processedRanges: string[] = []
+  const count = getIds(idRanges, '', 0);
 
-    let freshIDsCount = 0;
-
-    
-
-    for(const range of idRanges) {
-        const min = Number(range.split('-')[0]);
-        const max = Number(range.split('-')[1]);
-
-        if(processedRanges.length === 0) {
-            processedRanges.push(range);
-        } else {
-            processedRanges = updateRanges(processedRanges, range);
-        }
-    }
-
-    
-        console.log(processedRanges);
+  console.log(count);
 }
 
 solution();
